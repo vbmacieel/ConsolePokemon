12f72e1a3be9e9800f7c0c8f74bc4ab2faa127c8
Victor Bastos <vbastosmaciel@outlook.com.br>
2023-01-18 11:29:43 -0300

    First commit

---
+++ ConsolePokemon/Program.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+
+namespace ConsolePokemon
+{
+    public class Program
+    {
+        private const string UrlPokeApi = "https://pokeapi.co/api/v2/pokemon";
+
+        private static void Main(string[] args)
+        {
+            PokeApiGet();
+        }
+
+        private static void PokeApiGet()
+        {
+            RestClient client = new RestClient(UrlPokeApi);
+            RestRequest request = new RestRequest("", Method.Get);
+            RestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(response.Content);
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+        }
+    }
+}
